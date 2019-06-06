aa20a429406ba6cdf529627bb4036d6863d08117
unknown <82imjo1bif@hft-stuttgart.de>
2019-06-06 10:57:46 +0200

    add stock functionality +/-

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser')
@@ -8,10 +9,14 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true })); //nötig?
-app.use(bodyParser.json());   //nötig?
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(process.cwd() + '/public'));
+
+
+
+const TABLE_NAME = 'yas'
 
 app.get('/api/bestand', async (req, res) => {
   db.all('SELECT * FROM stockElements', (err, stockElements) => {
@@ -19,12 +24,27 @@ app.get('/api/bestand', async (req, res) => {
   });
 });   //datenbankabfrage
 
+
+// Index Route
 app.get('/', (req, res) => {
   res.render('pages/index', { success: true });
 });
+
+
+// Stock Route
 app.get('/stock', (req, res) => {
-  res.render('pages/bestand', { success: true });
+  db.all('SELECT * FROM Stock', (err, stockElements) => {
+    //TODO
+    if (err) {
+      console.log(err)
+      res.render('pages/bestand', { stockElements: [] })
+    }
+    res.render('pages/bestand', { stockElements });
+  });
 });
+
+
+// List Route
 app.get('/list', (req, res) => {
   res.render('pages/einkaufsliste', { success: true });
 });
@@ -32,26 +52,49 @@ app.get('/recipe', (req, res) => {
   res.render('pages/rezepte', { success: true });
 });
 
-app.post('/api/PLACEHOLDER', (req, res) => {
-  if (req.body.username && req.body.message) {
-    db.run('INSERT INTO shouts(username, message) VALUES (?, ?);', [req.body.username, req.body.message], function(err) {
-      if(err) {
-        res.status(500).end()
-      } else {
-        res.json({
-          "id" : this.lastID,
-          "username" : req.body.username,
-          "message" : req.body.message
-        }).end()
+app.post('/api/stock', (req, res) => {
+  if (req.body.name && req.body.amountStock) {
+    db.get('SELECT name,amountStock FROM stock WHERE name=?', [req.body.name], function (err,row) {
+      if (err) {
+        res.json({ msg: "error"})
+      }else{
+        console.log(row)
+        if(row.amountStock > 0){
+          updateStock({
+            body: {amount: row.amountStock, change: req.body.amountStock, name: req.body.name}
+          }, res);
+        }else{
+          res.json({ msg: "error"})
+        }
       }
-    });
+    })
   } else {
-    res.status(500).end()
+    console.log("Übergabeparameter null")
   }
-});   //Datenbankeintrag
-
-const server = app.listen(port, () => {
- console.log(`Server listening on port ${port}…`)
 });
 
+function updateStock(req, res) {
+  console.log(req.body)
+  const newAmount = req.body.amount + req.body.change;
+  db.run('UPDATE stock SET amountStock=? WHERE name=?;', [newAmount, req.body.name], function (err) {
+    if (err) {
+      res.json({ msg: "error", err})
+    }
+    else {
+      res.json({
+        "name": req.body.name,
+        "amount": newAmount
+      }).end();
+    }
+  });
+}
+
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}…`)
+});
+
+
+
 module.exports = server
+
+
--- public/script.js
+++ public/script.js
@@ -1,5 +1,28 @@
-$( document ).ready((() => {
+$(document).ready((() => {
     console.log('DOM is ready!')
-
-    
+    $('.add').on("click", (evt) => stockUpdate(evt, 1))
+    $('.remove').on("click", (evt) => stockUpdate(evt, -1))
 }))
+
+
+
+async function stockUpdate(evt, change) {
+    evt.preventDefault()
+    console.log(evt)
+    console.log(evt.currentTarget.dataset.name)
+    console.log(evt.currentTarget.dataset.amount)
+    const response = await fetch('/api/stock', {
+        method: "post",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+          },
+        body: JSON.stringify({
+            name: evt.currentTarget.dataset.name,
+            amountStock: change
+        })
+    })
+
+    const data = await response.json()
+    console.log(data)
+
+}

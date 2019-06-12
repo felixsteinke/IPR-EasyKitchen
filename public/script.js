29097b3b0a595405595e3eb5947ec9ec937deece
unknown <82imjo1bif@hft-stuttgart.de>
2019-06-12 17:37:42 +0200

    work on shopping list

--- public/script.js
+++ public/script.js
@@ -4,33 +4,60 @@ $(document).ready((() => {
     $('.remove').on("click", evt => stockUpdate(evt, -1))
     $('.btn-recipe').on('click', evt => openRecipe(evt))
     $('.btn-list').on('click', evt => addList(evt))
+    //$('.add-stock').on('click', evt => addBestand(evt))
+    //$('.decrease').on('click', evt => decreaseList(evt))
 }))
-
-function addList(evt){
+//list
+function decreaseList(evt){
+    evt.preventDefault()
+    console.log("called decreaseList()")
+}
+function addBestand(evt){
+    evt.preventDefault()
+    console.log("called addBestand()")
+}
+function addList(evt) {
     evt.preventDefault()
     const table = document.getElementById('body-shopping-list')
+    if (table.childElementCount != 0) {
+        for(i = 1; i<=table.childElementCount; i++){
+            const children = table.childNodes[i].childNodes
+            if (children[0].textContent == evt.currentTarget.dataset.name) {
+                children[2].textContent = parseInt(children[2].textContent)+1
+                return
+            }
+        }
+    }
+
+    addRow(table, evt.currentTarget.dataset)
+}
+function addRow(table, dataset){
     const row = table.insertRow()
-    row.insertCell(0).textContent = evt.currentTarget.dataset.name
-    row.insertCell(1).textContent = evt.currentTarget.dataset.amount
+    row.insertCell(0).textContent = dataset.name
+    row.insertCell(1).textContent = dataset.amount
     row.insertCell(2).textContent = "0"
+
+    //const test = '<a href="/list?special=data" type="button" id=dataset.name class="btn-size decrease btn btn-outline-secondary" data-name=dataset.name>-</a>'
     const btn = document.createElement('a')
     btn.type = 'button'
     btn.href = '/list?special=data'
-    btn.dataset.name = evt.currentTarget.name
-    btn.dataset.change = -1
-    btn.class = 'evt.currentTarget.name btn-size btn btn-outline-secondary'
+    btn.dataset.name = dataset.name
+    btn.id = dataset.name
+    btn.class = 'btn-size decrease btn btn-outline-secondary'
     btn.textContent = '-'
     row.insertCell(3).appendChild(btn)
+
     const btn2 = document.createElement('a')
     btn2.type = 'button'
     btn2.href = '/list?special=data'
-    btn2.dataset.name = evt.currentTarget.name
+    btn2.dataset.name = dataset.name
     btn2.dataset.change = 1
-    btn2.class = 'evt.currentTarget.name btn-size btn btn-outline-secondary'
-    btn2.textContent = '+'
+    btn2.id = dataset.name
+    btn2.class = 'btn-size add-stock btn btn-outline-secondary'
+    btn2.textContent = 'eingekauft'
     row.insertCell(4).appendChild(btn2)
 }
-
+//recipe
 async function openRecipe(evt) {
     evt.preventDefault()
     const div = document.getElementById('displayRecipe')
@@ -55,23 +82,16 @@ function displayRecipe(evt, data) {
     $("#components tr").remove()
     const jsObj = JSON.parse(comp)
     var i = 0
-    for(key in jsObj){
+    for (key in jsObj) {
         const row = table.insertRow(i)
         row.insertCell(0).textContent = key
         row.insertCell(1).textContent = jsObj[key]
         row.insertCell(2).textContent = "Gramm"
-        i = i+1
+        i = i + 1
     }
-    i=0
+    i = 0
 }
-
-function updateRow(data, newAmount) {
-    const row = document.getElementById(data.name).childNodes
-    row[3].textContent = newAmount
-    row[5].childNodes[0].dataset.amount = newAmount
-    row[7].childNodes[0].dataset.amount = newAmount
-}
-
+//stock
 async function stockUpdate(evt, change) {
     evt.preventDefault()
     if (evt.currentTarget.dataset.amount == 0 && change == -1) {
@@ -90,4 +110,10 @@ async function stockUpdate(evt, change) {
         const data = await response.json()
         updateRow(evt.currentTarget.dataset, data.amount)
     }
+}
+function updateRow(data, newAmount) {
+    const row = document.getElementById(data.name).childNodes
+    row[3].textContent = newAmount
+    row[5].childNodes[0].dataset.amount = newAmount
+    row[7].childNodes[0].dataset.amount = newAmount
 }
